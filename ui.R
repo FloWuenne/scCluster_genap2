9651fec80826d20dd77b05ededa70ff0a6f16dbc
FloWuenne <flowuenne@gmail.com>
2019-04-10 22:14:33 -0400

    experimented with app interface.

--- ui.R
+++ ui.R
@@ -1,33 +1,43 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
+#source("../uifunctions.R")
+library(DT)
+library(shinycssloaders)
 library(shiny)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("Explore gene expression in the mouse heart"),
+  br(),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
+      textInput("user_gene_clustering", label = "Enter Genesymbol", value = "Gapdh"),
+      
+      ## Dropdown menu for color panel
+      selectInput(
+        inputId="color_palette", label="Choose a color palette for expression!", 
+        choices=c("A","B","C","D","E"),
+        selected="D", multiple=FALSE, selectize=FALSE),
+      
+      ## Scale menu for point size 
+      sliderInput(
+        inputId="point_size", label="Change cell point size!", 
+        min =1, max = 6, value = 3, step = 1, round = TRUE) 
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
-       plotOutput("distPlot")
+      tabsetPanel(
+        tabPanel("Plot",
+                 textOutput("default")
+        
+      )
+      
     )
   )
-))
+  
+  )
+)
+)
